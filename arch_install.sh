99fed895e5aaa9ba68deb2b1ec938854d0ccf1e0
Anurag Chandra <60315805+0x1r@users.noreply.github.com>
2025-04-29 03:52:33 +0530

    dhcpcd
    for dynamic ipv6 and ipv4 (poco f5) usb tether wasnt working because it shows us as mobile broadband

--- arch_install.sh
+++ arch_install.sh
@@ -63,7 +63,7 @@ mkinitcpio -p linux
 passwd
 # echo root:password | chpasswd
 # pacman --noconfirm -S grub efibootmgr os-prober
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils  alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  virt-manager qemu edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font tlp
+pacman -S grub efibootmgr dhcpcd networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils  alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  virt-manager qemu edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font tlp
 
 echo "Enter EFI partition: " 
 read efipartition
@@ -80,6 +80,7 @@ systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable reflector.timer
 systemctl enable tlp 
+systemctl enable dhcpcd
 # systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
