b92f670f1b1276ab4eea8909605ff0be95e4e3c3
Anurag Chandra <60315805+0x1r@users.noreply.github.com>
2025-05-19 09:58:43 +0530

    Update zshsetatno3.sh

--- zshsetatno3.sh
+++ zshsetatno3.sh
@@ -9,8 +9,9 @@ sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-aut
 sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/$USER/.zshrc
 
 
+#use this style to set the highlight color of the autosuggestion on sweet konsole theme
 
-
+#ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#757575'
 
 #append without NOPASSWD
 sudo echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
