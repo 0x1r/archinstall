6d23b6fb44af6dfe13be76a50e4fcfa173a09129
Anurag Chandra <60315805+0x1r@users.noreply.github.com>
2025-05-19 09:53:01 +0530

    Update zshsetatno3.sh
    user to target builtin variable for the current user

--- zshsetatno3.sh
+++ zshsetatno3.sh
@@ -5,8 +5,9 @@ echo "Enter Username: "
 read username
 
 #adding plugin and extention to .zshrc
-sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' /home/$username/.zshrc
-sed -i -e "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" /home/$username/.zshrc
+sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' /home/$USER/.zshrc
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/$USER/.zshrc
+
 
 
 
